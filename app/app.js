e02568030b53cc8f184c751f15d95027b29a18b6
Hristo Spasov <hristo_sp99@abv.bg>
2017-11-22 22:43:11 +0200

    Add webpack, react and semantic ui to project

---
+++ app/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
+
+import AppContainer from "./container/app-container";
+
+ReactDOM.render(
+    <AppContainer />,
+    document.getElementById("content")
+);
