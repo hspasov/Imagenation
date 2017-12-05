267d04d7518bc00925e227f85b9868360a4912e8
Hristo Spasov <hristo_sp99@abv.bg>
2017-12-05 23:44:08 +0200

    Curves implemented

--- app/app.js
+++ app/app.js
@@ -1,67 +1,116 @@
-const Caman = require("caman").Caman;
 const fs = require("fs");
-const url = require("url");
 const Canvas = require("canvas");
-const path = require("path");
-window.$ = require("jquery");
 
-
-let canv;
+const canv = fx.canvas();
 let texture;
-let base64Url;
-window.onload = function () {
-    try {
-        canv = fx.canvas();
-    } catch (e) {
-        alert(e);
-        return;
+let imageDownloadLink;
+
+// Set default values
+const curvesValues = {
+    red: {
+        0: [0, 0],
+        1: [1, 1]
+    }, green: {
+        0: [0, 0],
+        1: [1, 1]
+    }, blue: {
+        0: [0, 0],
+        1: [1, 1]
+    }, luminance: {
+        0: [0, 0],
+        1: [1, 1]
     }
+};
+
+document.getElementById("imageUpload").addEventListener("change", onImageUpload);
+
+Array.from(document.querySelectorAll("[type='range']"))
+    .forEach(element => element.addEventListener("change", sliderChange));
+
+document.getElementById("downloadButton").addEventListener("click", onDownload);
+
+document.getElementById("reset").addEventListener("click", setDefault);
+
+Array.from(document.getElementsByClassName("addPoint"))
+    .forEach(element => element.addEventListener("click", onAddPoint));
+
+function onImageUpload(event) {
+    const imgPath = event.target.files[0].path;
+    loadImage(imgPath);
 }
 
-$("#imageUpload").change((event) => {
-    const imgPath = event.target.files[0].path;
-    $("#brighten").click(() => {
-        canv.draw(texture).curves([[0, 0.5], [1, 0]]).update();
-        base64Url = canv.toDataURL();
+function onDownload() {
+    const link = document.getElementById("imageDownloadLink");
+    link.href = imageDownloadLink;
+    link.download = "Untitled.png";
+    link.click();
+}
+
+function onAddPoint(event) {
+    const setting = event.target.parentElement.id;
+    const newDefaultValue = event.target.previousElementSibling.value;
+    curvesValues[setting][newDefaultValue] = [newDefaultValue, newDefaultValue];
+
+    const label = document.createElement("label");
+    label.innerHTML = newDefaultValue;
+
+    const slider = document.createElement("input");
+    slider.setAttribute("type", "range");
+    slider.setAttribute("min", 0);
+    slider.setAttribute("max", 1);
+    slider.setAttribute("value", newDefaultValue);
+    slider.setAttribute("step", 0.1);
+    slider.addEventListener("change", sliderChange);
+
+    const settingSection = document.getElementById(setting);
+    settingSection.appendChild(label);
+    settingSection.appendChild(slider);
+}
+
+function sliderChange(event) {
+    const setting = event.target.parentElement.id;
+    const defaultValue = event.target.defaultValue;
+    const newValue = event.target.value;
+    curvesValues[setting][defaultValue] = [defaultValue, newValue];
+    render();
+}
+
+function setDefault() {
+    Array.from(document.querySelectorAll("[type='range']")).forEach(element => {
+        const setting = element.parentElement.id;
+        const defaultValue = element.defaultValue;
+        element.value = defaultValue;
+        curvesValues[setting][defaultValue] = [defaultValue, defaultValue];
     });
-    $("#darken").click(() => {
-        canv.draw(texture).brightnessContrast(-0.25, 0).update();
-        base64Url = canv.toDataURL();
-    });
-    $("#download").click(() => {
-        const link = document.getElementById("downloadLink");
-        link.href = base64Url;
-        link.download = "happy.png";
-        link.innerHTML = "Download";
-    });
-    loadImage(imgPath);
-});
+    render();
+}
+
+function render() {
+    canv.draw(texture).curves(
+        Object.values(curvesValues.red),
+        Object.values(curvesValues.green),
+        Object.values(curvesValues.blue)
+    ).curves(Object.values(curvesValues.luminance)).update();
+    imageDownloadLink = canv.toDataURL();
+}
 
 function loadImage(path) {
-    console.log("here in displayImage with path", path);
     fs.readFile(path, (err, data) => {
         if (err) throw err;
-        let img = new Canvas.Image;
+        const img = new Canvas.Image;
         img.onload = () => {
-            let canvas_ = new Canvas(500, 500);
-            let ctx = canvas_.getContext("2d");
+            const canvas_ = new Canvas(500, 500);
+            const ctx = canvas_.getContext("2d");
             ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, 500, 500);
-            let image = document.createElement("img");
+            const image = document.createElement("img");
             image.onload = () => {
                 texture = canv.texture(image);
                 canv.draw(texture).update();
-                $("#imageHolder").append(canv);
-                base64Url = canv.toDataURL();
+                document.getElementById("imageHolder").appendChild(canv);
+                imageDownloadLink = canv.toDataURL();
             }
             image.src = canvas_.toDataURL();
         };
         img.src = data;
     });
-}
-
-function sleep(miliseconds) {
-    var currentTime = new Date().getTime();
-
-    while (currentTime + miliseconds >= new Date().getTime()) {
-    }
 }
